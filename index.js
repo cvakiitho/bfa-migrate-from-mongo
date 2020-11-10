1b9ea37ca16601deaebe4923547e564d199cf2d5
Tomas Hartmann <tomas.hartmann@sap.com>
2020-11-10 20:06:13 +0100

    start adding categories

--- index.js
+++ index.js
@@ -1,83 +1,105 @@
-const { toXML } = require('jstoxml');
+const {
+  toXML
+} = require('jstoxml');
 const fs = require('fs');
 var jsonFail = require('./failure_cause.js').kb
-function uuid()
-{
-   var chars = '0123456789abcdef'.split('');
 
-   var uuid = [], rnd = Math.random, r;
-   uuid[8] = uuid[13] = uuid[18] = uuid[23] = '-';
-   uuid[14] = '4'; // version 4
+function uuid() {
+  var chars = '0123456789abcdef'.split('');
 
-   for (var i = 0; i < 36; i++)
-   {
-      if (!uuid[i])
-      {
-         r = 0 | rnd()*16;
+  var uuid = [],
+    rnd = Math.random,
+    r;
+  uuid[8] = uuid[13] = uuid[18] = uuid[23] = '-';
+  uuid[14] = '4'; // version 4
 
-         uuid[i] = chars[(i == 19) ? (r & 0x3) | 0x8 : r & 0xf];
-      }
-   }
+  for (var i = 0; i < 36; i++) {
+    if (!uuid[i]) {
+      r = 0 | rnd() * 16;
 
-   return uuid.join('');
+      uuid[i] = chars[(i == 19) ? (r & 0x3) | 0x8 : r & 0xf];
+    }
+  }
+
+  return uuid.join('');
 }
 
 
 let result = new Object([])
-for (el of jsonFail){
-    try {
-        delete el.lastOccurred
-        delete el._id
-        delete el._removed
-        delete el.modifications
-        delete el.comment
-        //fix indications
-        for (ind of el.indications){
-            ind._name = ind['@class']
-            delete ind['@class']
-            ind._content = {
-                _name: 'pattern',
-                _content: ind.pattern
-            }
-            delete(ind.pattern)
-        }
+for (let el of jsonFail) {
+  try {
+    delete el.lastOccurred
+    delete el._id
+    delete el._removed
+    delete el.modifications
+    delete el.comment
+    //fix indications
+    for (let ind of el.indications) {
+      ind._name = ind['@class']
+      delete ind['@class']
+      ind._content = {
+        _name: 'pattern',
+        _content: ind.pattern
+      }
+      delete(ind.pattern)
     }
-        catch (e){console.error(e)}
+  } catch (e) {
+    console.error(e)
+  }
 
-    let temp = Object.assign(el)
-    let id = uuid()
-    temp.id = id
+  //rework category so it generated
+  // <categories class="java.util.Arrays$ArrayList">                                                                                                                                                                     
+  // <a class="string-array">                                                                           
+  // <string>BQ5</string>                                                                             
+  // </a>                                                                                               
+  // </categories>
+  if(el.categories) {
 
-    //structure
-    result.push({
-        _name: 'causes',
-        _content: {
-          _name: 'entry',
-          _content: [{
-            _name: 'com.sonyericsson.jenkins.plugins.bfa.model.FailureCause',
-            _content: temp
-          },
-              {
-                  string: id
-              }
-          ]
+    let resultCategory = {}
+    resultCategory._content = Object.assign(el.categories)
+    resultCategory._name = 'categories'
+    resultCategory._attrs = {class:'java.util.Arrays$ArrayList'}
+    console.dir(resultCategory)
+    el.categories = resultCategory
+
+  }
+
+  //uuid 
+  let temp = Object.assign(el)
+  let id = uuid()
+  temp.id = id
+
+  //structure
+  result.push({
+    _name: 'causes',
+    _content: {
+      _name: 'entry',
+      _content: [
+        {
+          string: id
+        },
+        {  _name: 'com.sonyericsson.jenkins.plugins.bfa.model.FailureCause',
+          _content: temp
         }
+      ]
+    }
 
-    })
-    console.dir(result)
+  })
 }
 
-//console.dir(jsonFail)
-//
-//
 const xmlOptions = {
-      header: false,
-      indent: '  '
+  header: false,
+  indent: '  '
 };
 
 var xml = toXML(result, xmlOptions)
 
 var header = `<?xml version='1.1' encoding='UTF-8'?>
+<com.sonyericsson.jenkins.plugins.bfa.PluginImpl plugin="build-failure-analyzer@1.27.1">
+  <noCausesMessage>No problems were identified. If you know why this problem occurred, please add a suitable Cause for it.</noCausesMessage>
+  <doNotAnalyzeAbortedJob>false</doNotAnalyzeAbortedJob>
+  <knowledgeBase class="com.sonyericsson.jenkins.plugins.bfa.db.LocalFileKnowledgeBase">`
+var brqheader = `<?xml version='1.1' encoding='UTF-8'?>
 <com.sonyericsson.jenkins.plugins.bfa.PluginImpl plugin="build-failure-analyzer@1.27.1">
   <noCausesEnabled>true</noCausesEnabled>
   <noCausesMessage>No problems were identified. If you know why this problem occurred, please add a suitable Cause for it.</noCausesMessage>
@@ -90,7 +112,20 @@ var header = `<?xml version='1.1' encoding='UTF-8'?>
   <fallbackCategoriesAsString></fallbackCategoriesAsString>
   <knowledgeBase class="com.sonyericsson.jenkins.plugins.bfa.db.LocalFileKnowledgeBase">
 `
-var footer = `</knowledgeBase>
+var footer = `  </knowledgeBase>
+<nrOfScanThreads>3</nrOfScanThreads>
+<maxLogSize>0</maxLogSize>
+<testResultCategories></testResultCategories>
+<sodVariables>
+  <minimumSodWorkerThreads>0</minimumSodWorkerThreads>
+  <maximumSodWorkerThreads>0</maximumSodWorkerThreads>
+  <sodThreadKeepAliveTime>0</sodThreadKeepAliveTime>
+  <sodWaitForJobShutdownTimeout>0</sodWaitForJobShutdownTimeout>
+  <sodCorePoolNumberOfThreads>0</sodCorePoolNumberOfThreads>
+</sodVariables>
+</com.sonyericsson.jenkins.plugins.bfa.PluginImpl>`
+
+var brqfooter = `</knowledgeBase>
   <nrOfScanThreads>3</nrOfScanThreads>
   <maxLogSize>0</maxLogSize>
   <graphsEnabled>false</graphsEnabled>
@@ -108,6 +143,3 @@ var footer = `</knowledgeBase>
 
 
 fs.writeFileSync('./result.xml', header + xml + footer)
-
-
-
